adeb2e50671621b16c0525de52a0ad98ee8daa18
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-05-10 00:12:12 +0530

    Updates

---
+++ precache-manifest.bba756cead3607ad482191ce858ed1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ba72c7864954050a6d5735d1319923",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "845947550c31323b0e62",
+    "url": "/weather-app/static/css/main.33eee306.chunk.css"
+  },
+  {
+    "revision": "28d088d11670d466333c",
+    "url": "/weather-app/static/js/2.140ca6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.140ca6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845947550c31323b0e62",
+    "url": "/weather-app/static/js/main.8f95b831.chunk.js"
+  },
+  {
+    "revision": "82e98bb84252aced605d",
+    "url": "/weather-app/static/js/runtime-main.c3c42d5c.js"
+  }
+]);
