096605c12fe014081faf85b9463a4fea378fcf03
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-05-13 00:01:27 +0530

    Hash Routed

--- src/components/RoutePack/RoutePack.js
+++ src/components/RoutePack/RoutePack.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "../Navbar/Navbar.js";
 import Main from "../Main/Main.js";
 import MyCities from "../MyCities/MyCities.js";
